d6cd2fa926198a3c045d511b11c04e970ab7746d
stasstels <stanislav.oispov@sidenis.com>
2015-06-23 17:34:17 +0300

    Functional test update

--- book/movieman/functional_tests.py
+++ book/movieman/functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -14,6 +15,24 @@ class NewVisitorTest(unittest.TestCase):
 		self.browser.get('http://localhost:8000')
 		
 		self.assertIn('To-Do', self.browser.title)
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
+
+		#Now it's possible to enter movie to see
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(inputbox.get_attribute('placeholder'), "Enter a movie name")
+		
+		#User types Watchmen into text box
+		inputbox.send_keys('Watchmen')
+		#Hit the enter
+		inputbox.send_keys(Keys.ENTER)
+
+		table = self.browser.find_element_by_id('movie_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(any(row.text == '1: Watchmen' for row in rows))
+
+
+
 		self.fail('Fail!')
 
 if __name__ == '__main__':
