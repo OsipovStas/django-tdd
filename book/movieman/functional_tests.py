8b850dc537d20f8d9fe80ae5a6357d8397054165
stasstels <stanislav.oispov@sidenis.com>
2015-06-26 15:37:43 +0300

    refactor FT

--- book/movieman/functional_tests.py
+++ book/movieman/functional_tests.py
@@ -11,6 +11,13 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+
+	def check_for_row_in_movie_list_table(self, row_text):
+		table = self.browser.find_element_by_id('movie_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
+
 	def test_that_can_start_a_list_and_retrieve_it_later(self):
 		self.browser.get('http://localhost:8000')
 		
@@ -29,8 +36,8 @@ class NewVisitorTest(unittest.TestCase):
 
 		table = self.browser.find_element_by_id('movie_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1. Watchmen', [row.text for row in rows])
-		self.assertIn('2. The Matrix', [row.text for row in rows])
+		self.check_for_row_in_movie_list_table('1. Watchmen')
+		self.check_for_row_in_movie_list_table('2. The Matrix')
 
 		self.fail('Fail!')
 
